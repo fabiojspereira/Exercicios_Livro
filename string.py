4c53f6332bbc92dcd15f8bb882f29b32f6491f35
Fabio JS Pereira <fabiojspereira@gmail.com>
2021-12-01 22:59:47 -0300

    exercicios iniciais do livro

---
+++ string.py
@@ -0,0 +1,16 @@
+name = "fabio jorge de souza pereira"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "fabio\n"
+last_name = "pereira\n"
+full_name = first_name + last_name
+message = ("Olá\n" + full_name.title())
+print(message)
+
+
+
+
+
+
