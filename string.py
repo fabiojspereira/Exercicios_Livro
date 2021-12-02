ffaeac0ce7bf6d00e0b0fdde26f0789f51bc3ce7
Fabio JS Pereira <fabiojspereira@gmail.com>
2021-12-02 20:12:07 -0300

    Update

--- string.py
+++ string.py
@@ -1,3 +1,31 @@
+nome = "Fabio"
+print("Olá " + nome + ", você gostaria de aprender um pouco sobre Python hoje ?")
+
+nome = "fabio jorge de souza pereira"
+print(nome.title())
+
+citacao = 'Albert Einstein certa vez disse : " Uma pessoa que nunca cometeu um erro jamais tentou nada novo. "'
+print(citacao)
+
+famous_person = "Albert Einstein"
+mesage = ("\n"+famous_person.title() + ' certa vez disse : " Uma pessoa que nunca cometeu um erro jamais tentou nada novo. "')
+print(mesage)
+
+nome = " Fabio Pereira             "
+print(nome.rstrip())
+print("\t",nome.lstrip())
+print("\n",nome.strip())
+
+
+'''
+texto1 = "FABIO     "
+texto2 = "    PEREIRA"
+print(texto1 + texto2)
+
+texto1 = texto1.rstrip()
+texto2 = texto2.lstrip()
+print(texto1+texto2)
+
 name = "fabio jorge de souza pereira"
 print(name.title())
 print(name.upper())
@@ -8,3 +36,4 @@ last_name = "pereira\n"
 full_name = first_name + last_name
 message = ("Olá\n" + full_name.title())
 print(message)
+'''
