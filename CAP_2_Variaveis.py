0c59c8ac72e8862c9199a53b655f16273997db77
Fabio JS Pereira <fabiojspereira@gmail.com>
2022-01-18 15:44:51 -0300

    Reorganizando o repositorio Exercicios_Livro

--- CAP_2_Variaveis.py
+++ CAP_2_Variaveis.py
@@ -1,30 +1,48 @@
+
 nome = "Fabio"
 print("Olá " + nome + ", você gostaria de aprender um pouco sobre Python hoje ?")
 
 nome = "fabio jorge de souza pereira"
 print(nome.title())
+print(nome.lower())
+print(nome.upper())
+
 
 citacao = 'Albert Einstein certa vez disse : " Uma pessoa que nunca cometeu um erro jamais tentou nada novo. "'
 print(citacao)
 
-famous_person = "Albert Einstein"
+famous_person = "albert einstein"
 mesage = ("\n"+famous_person.title() + ' certa vez disse : " Uma pessoa que nunca cometeu um erro jamais tentou nada novo. "')
 print(mesage)
 
-nome = " Fabio Pereira             "
-print(nome.rstrip())
-print("\t",nome.lstrip())
-print("\n",nome.strip())
+pre = "Ola"
+nome = "   fabio            "
+pos = "Tchau"
+print(pre,nome,pos)
+print(nome.rstrip(),pos)
+print(pre,nome.lstrip())
+print(pre,nome.strip(),pos)
+
+numero = 9
+print("Meu número favorito é :",numero)
+print("Meu número favorito é : "+ str(numero))
 
 
-'''
-texto1 = "FABIO     "
+BLOCO = "XXX"
+
+nome = "Fabio"
+print(nome.rstrip()+BLOCO,"\n")
+print(BLOCO,nome.lstrip(),BLOCO,"\n")
+print(BLOCO,nome.strip(),BLOCO,"\n")
+
+
+texto1 = "FABIO      "
 texto2 = "    PEREIRA"
-print(texto1 + texto2)
+print(texto1 + texto2,"\n")
 
 texto1 = texto1.rstrip()
 texto2 = texto2.lstrip()
-print(texto1+texto2)
+print(texto1+texto2,"\n")
 
 name = "fabio jorge de souza pereira"
 print(name.title())
@@ -36,4 +54,4 @@ last_name = "pereira\n"
 full_name = first_name + last_name
 message = ("Olá\n" + full_name.title())
 print(message)
-'''
+
---
+++ CAP_3_Listas.py
@@ -0,0 +1,9 @@
+lista_bicicletas = ["trek","cannondale","redline","specialized" ]
+print(lista_bicicletas)
+print(lista_bicicletas[0].title())
+print(lista_bicicletas[3].title())
+print(lista_bicicletas[-1].title())
+print(lista_bicicletas[-4].title())
+
+txt = ("Minha primeira bicicleta foi uma " + lista_bicicletas[0].title() + ".")
+print (txt)
