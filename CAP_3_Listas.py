a1559f267fe1f1cfb3610f3d9ec86ec11a02821d
Fabio JS Pereira <fabiojspereira@gmail.com>
2022-03-10 19:44:33 -0300

    CAP_3_Listas

--- CAP_3_Listas.py
+++ CAP_3_Listas.py
@@ -130,9 +130,9 @@ print(soma)
 print(lista_bicicletas)
 
 print(lista_bicicletas[0].title())
-print(lista_bicicletas[3].title())
+print(lista_bicicletas[2].title())
 print(lista_bicicletas[-1].title())
-print(lista_bicicletas[-4].title())
+print(lista_bicicletas[-3].title())
 
 txt = ("Minha primeira bicicleta foi uma " + lista_bicicletas[0].title() + ".")
 print (txt)
