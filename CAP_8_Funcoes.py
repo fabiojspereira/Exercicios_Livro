2933d4ddbe2d770ee5c5eba13ca2e524dbf5ed48
fabiojspereira <fabiojspereira@gmail.com>
2022-04-06 13:51:38 -0300

    Continuação dos exercícios do livro - Funcoes

--- CAP_8_Funcoes.py
+++ CAP_8_Funcoes.py
@@ -1,4 +1,4 @@
-"""
+
 def build_person(first_name, last_name, age=""):
 	person = {"first":first_name, "last":last_name}
 	if age:
@@ -15,7 +15,7 @@ musician = build_person("jimi", "hendrix", 42)
 print(musician)
 
 
-
+"""
 def build_person(first_name, last_name):
 	person = {"first":first_name, "last":last_name}
 	return person
